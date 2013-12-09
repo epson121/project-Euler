4deb8145b5bdc1ea18a2bf25b14f339d41294ad1
epson121 <traxdater12@gmail.com>
2013-12-09 21:16:00 +0100

    Small optimization in Euler7

--- Euler7.java
+++ Euler7.java
@@ -4,28 +4,28 @@ class Euler7
 {
     public static void main(String[] args)
     {
-        int limit = 1000000;
+        int limit = 10001;
         int counter = 0;
         int res = 0;
         int i = 1;
         boolean found = false;
-            for (int k = 3; ;k++ ) {
-                if (k % 2 == 0)
-                    continue;
-                for (int j = 3; j <= Math.sqrt(k); j+=2) {
-                    //System.out.println(j);
-                    if (k % j == 0) {
-                        found = true;
-                    }
-                }
-                if (!found)
-                    i += 1;
-                else
-                    found = false;
-                if (i == limit) {
-                    System.out.println("N: " + k);
+        for (int k = 3; ;k++ ) {
+            if (k % 2 == 0)
+                continue;
+            for (int j = 3; j <= Math.sqrt(k); j+=2) {
+                if (k % j == 0) {
+                    found = true;
                     break;
                 }
-            }            
+            }
+            if (!found)
+                i += 1;
+            else
+                found = false;
+            if (i == limit) {
+                System.out.println("N: " + k);
+                break;
+            }
+        }            
     }
 }
